3d1b571f454fdb775a7e886ee836eceaa3151679
diablo0897 <81802586+diablo0897@users.noreply.github.com>
2021-04-27 18:52:16 -0400

    4/27

--- js/script.js
+++ js/script.js
@@ -15,6 +15,9 @@ $(function(){ //same as document.addEventListener("DOMContentLoaded"...)//
     var allCatagoriesUrl = "http://davids-restaurant.herokuapp.com/categories.json";
     var categoriesTitleHtml = "snippets/categories-title-snippet.html";
     var categoryHtml = "snippets/category-snippet.html"
+    const menuItemsUrl = "http://davids-restaurant.herokuapp.com/menu_items.json?category=";
+    const menuItemsTitleHtml = "snippets/menu-items-title.html"
+    const menuItemHtml = "snippets/menu-item.html"
 
     //Inserting innterHTML for 'select'
     var insertHtml = function (selector,html) {
@@ -83,6 +86,88 @@ $(function(){ //same as document.addEventListener("DOMContentLoaded"...)//
         }
         console.log(finalHtml)
         return finalHtml
+    }
+
+    //Load the menu items view
+    //'categoryShort' is the short_name for a category
+    dc.loadMenuItems = function (categoryShort) {
+        showLoading("#main-content")
+        $ajaxUtils.sendGetRequest(menuItemsUrl+categoryShort,buildAndShowMenuItemHTML);
+    };
+
+    function buildAndShowMenuItemHTML(categoryMenuItems) {
+        //Load menu item title snippet first
+        $ajaxUtils.sendGetRequest(menuItemsTitleHtml,function (menuItemTitleHtml) {
+            //Retrieve single item snippet
+            $ajaxUtils.sendGetRequest(menuItemHtml,function (menuItemHtml) {
+                let menuItemsViewHtml = buildMenuItemsViewHtml(categoryMenuItems,menuItemsTitleHtml,menuItemHtml);
+                insertHtml("#main-content",menuItemsViewHtml);
+            },false);
+        },false);
+    }
+
+
+
+    //Use category and menu items data and snippets html to build menu items view HTML
+    function buildMenuItemsViewHtml(categoryMenuItems,menuItemsTitleHtml,menuItemHtml) {
+        menuItemsTitleHtml = insertProperty(menuItemsTitleHtml,"name",categoryMenuItems.category.name);
+        menuItemsTitleHtml = insertProperty(menuItemsTitleHtml,"special_instructions",categoryMenuItems.category.special_instructions)
+        
+        let finalHtml = menuItemsTitleHtml;
+        finalHtml+="<section class='row'>";
+        
+        let menuItems = categoryMenuItems.menu_items;
+        let catShortName = categoryMenuItems.category.short_name;
+        function handleSingleMenuItemView(previous,item,index) {
+
+            let html = menuItemHtml;
+            html = insertProperty(html,"short_name",item.short_name);
+            html = insertProperty(html,"catShortName",catShortName);
+            html = insertItemPrice(html,"price_small",item.price_small);
+            html = insertItemPortionName(html,"small_portion_name",item.small_portion_name);
+            html = insertItemPrice(html,"price_large",item.price_large);
+            html = insertItemPortionName(html,"large_protion_name",item.large_portion_name);
+            html = insertProperty(html,"name",item.name);
+            html = insertProperty(html,"description",item.description);
+
+            if (index%2 != 0) {
+                html += "<div class='clearfix d-lg-block'></div>"
+            };
+
+
+            return previous+html;
+
+        }
+        finalHtml= menuItems.reduce(handleSingleMenuItemView,finalHtml);
+        console.log("this is final: "+finalHtml);
+        return finalHtml;
+
+    }
+    // Appends price with '$' if price exists
+    function insertItemPrice(html,
+                             pricePropName,
+                             priceValue) {
+        // If not specified, replace with empty string
+        if (!priceValue) {
+            return insertProperty(html, pricePropName, "");;
+        }
+
+        priceValue = "$" + priceValue.toFixed(2);
+        html = insertProperty(html, pricePropName, priceValue);
+        return html;
+    }
+    // Appends portion name in parens if it exists
+    function insertItemPortionName(html,
+                                   portionPropName,
+                                   portionValue) {
+        // If not specified, return original string
+        if (!portionValue) {
+            return insertProperty(html, portionPropName, "");
+        }
+
+        portionValue = "(" + portionValue + ")";
+        html = insertProperty(html, portionPropName, portionValue);
+        return html;
     }
 
 
