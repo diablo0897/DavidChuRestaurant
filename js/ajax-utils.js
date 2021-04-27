02f40074eccd088121d94270bbf62475a178406d
diablo0897 <81802586+diablo0897@users.noreply.github.com>
2021-04-27 15:05:07 -0400

    init commit

--- js/ajax-utils.js
+++ js/ajax-utils.js
@@ -18,18 +18,34 @@
     }
 
     //make an Ajax GET request to 'requestUrl'
-    ajaxUtils.sendGetRequest = function (requestUrl,responseHandler) {
+    ajaxUtils.sendGetRequest = function (requestUrl,responseHandler,isJsonResponse) {
         var request = getRequestObject();
         request.onreadystatechange = function () { //when the server is responsed call this function to handle it
-            handleResponse(request,responseHandler);
+
+            handleResponse(request,responseHandler,isJsonResponse);
         };
-        request.open("GET",request,true);
+
+        request.open("GET",requestUrl,true);
+
         request.send(null);//for POST request body
     }
     //Only calls user provided 'responseHandler' function if response is ready and not an error
-    function handleResponse(request,responseHandler){
+    function handleResponse(request,responseHandler,isJsonResponse){
+
         if ((request.readyState===4)&&(request.status===200)){
-            responseHandler(request);
+
+            //Default to isJsonResponse = true
+            if(isJsonResponse === undefined){
+
+                isJsonResponse = true;
+            }
+            if(isJsonResponse){
+
+                responseHandler(JSON.parse(request.responseText))
+            }else{
+
+                responseHandler(request.responseText)
+            }
         }
     }
     global.$ajaxUtils = ajaxUtils;
--- js/script.js
+++ js/script.js
@@ -12,6 +12,9 @@ $(function(){ //same as document.addEventListener("DOMContentLoaded"...)//
     var dc = {};
 
     var homeHtml = "snippets/home-snippet.html";
+    var allCatagoriesUrl = "http://davids-restaurant.herokuapp.com/categories.json";
+    var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+    var categoryHtml = "snippets/category-snippet.html"
 
     //Inserting innterHTML for 'select'
     var insertHtml = function (selector,html) {
@@ -25,9 +28,64 @@ $(function(){ //same as document.addEventListener("DOMContentLoaded"...)//
         insertHtml(selector,html)
     };
 
+    //Return subsititue of {{propName}} with propValue in given 'string'
+    var insertProperty =function (string,propName,propValue) {
+        var propToReplace = "{{"+propName+"}}"
+        string = string.replace(new RegExp(propToReplace,"g"),propValue);
+        return string;
+    }
+
     //on page load(before images or CSS)
     document.addEventListener("DOMContentLoaded",function (event) {
         //on first load, show loading screen
         showLoading("#main-content");
-    })
+        $ajaxUtils.sendGetRequest(homeHtml,function (responseText) {
+            document.querySelector("#main-content").innerHTML = responseText
+        },false);
+    });
+
+    //Load the menu categories view
+    dc.loadMenuCategories = function () {
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(allCatagoriesUrl,buildAndShowCategoriesHTML);
+        console.log(allCatagoriesUrl);
+    };
+
+    //Builds HTML for the categories page based on the data from server
+    function buildAndShowCategoriesHTML(categories) {
+        console.log("here")
+        //Load title snippet of categories page
+        $ajaxUtils.sendGetRequest(categoriesTitleHtml,function (categoriesTitleHtml) {
+            //Retrieve single category snippet
+            console.log(categoriesTitleHtml)
+            $ajaxUtils.sendGetRequest(categoryHtml,function (categoryHtml) {
+                console.log(categoryHtml)
+                var categoriesViewHtml = buildCategoriesViewHtml(categories,categoriesTitleHtml,categoryHtml);
+                insertHtml("#main-content",categoriesViewHtml)
+            },false);
+        },false);
+    }
+    function buildCategoriesViewHtml(categories,categoriesTitleHtml,categoryHtml){
+        console.log("start building")
+        var finalHtml = categoriesTitleHtml;
+        finalHtml+= "<section class='row'>";
+        console.log(finalHtml)
+
+        for(var i = 0; i < categories.length;i++){
+            var html = categoryHtml;
+            var name = ""+ categories[i].name;
+            var short_name = categories[i].short_name;
+            html = insertProperty(html,"name",name);
+            html = insertProperty(html,"short_name",short_name);
+            finalHtml+=html;
+            console.log(finalHtml)
+
+        }
+        console.log(finalHtml)
+        return finalHtml
+    }
+
+
+
+    global.$dc = dc;
 })(window);
